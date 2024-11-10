be796c6fc2d5dd81554ebf152acb6be543924ef5
Chrrrrris <1304426365@qq.com>
2024-11-10 14:17:04 -0500

    preprocessing

--- service.py
+++ service.py
@@ -21,15 +21,30 @@ def postureDetect(interval=60):
 # testing
 if __name__ == "__main__":
     # local storage path
-    data_path = r"D:\JHU24-25\MLSys\data"
+    data_path = "../AI System Data/Normal posture/frames/"
     # Get all files in the data_path folder
     image_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
 
-    images = [Image.open(os.path.join(data_path, image_file)) for image_file in image_files]
+    batch_size = 20
+    counter = 0
 
+    for i in range(len(image_files)//batch_size):
+      images = [Image.open(os.path.join(data_path, image_file)) for image_file in image_files[counter:counter+batch_size]]
+
+      results = postureDetect(images)
+      print(f"length of result is {len(results)}")
+
+      # assert len(results) == len(images)
+
+      # Print keypoints results
+      for idx, keypoints in enumerate(results):
+          print(f"Image {idx}: Keypoints - {keypoints}")
+      counter += batch_size
+
+          
+    images = [Image.open(os.path.join(data_path, image_file)) for image_file in image_files[counter:]]
     results = postureDetect(images)
-
-    assert len(results) == len(images)
+    # assert len(results) == len(images)
 
     # Print keypoints results
     for idx, keypoints in enumerate(results):
